1561eb3bcb2e0012befa4616feb1fc4a29144b05
MrInfuse <tjohndereck@gmail.com>
2020-09-17 07:17:26 +0800

    move: colors

---
+++ colors/one-color.h
@@ -0,0 +1,34 @@
+const char *colorname[] = {
+
+  /* 8 normal colors */
+	"#0c0f15",
+	"#B28C6E",
+	"#73878B",
+	"#A4A8A2",
+	"#C1C4B9",
+	"#CCD8D2",
+	"#D2E1DD",
+	"#e4ecec",
+
+/* 8 bright colors */
+	"#9fa5a5",
+	"#B28C6E",
+	"#73878B",
+	"#A4A8A2",
+	"#C1C4B9",
+	"#CCD8D2",
+	"#D2E1DD",
+	"#e4ecec",
+
+	[255] = 0,
+
+/* special colors */
+	"#0c0f15", /* background */
+	"#e4ecec", /* foreground */
+	"#e4ecec", /* cursor */
+};
+
+ unsigned int defaultbg = 256;
+ unsigned int defaultfg = 257;
+ unsigned int defaultcs = 258;
+ unsigned int defaultrcs= 258;
