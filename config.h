d38010deba0f335c1333712628b3bf89d058e4c5
john <john@Messier104.localdomain>
2020-10-09 20:19:39 +0800

    patched:   font2

--- config.h
+++ config.h
@@ -6,6 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Liberation Mono:pixelsize=16:antialias=true:autohint=true";
+static char *font2[] = { "JoyPixels:pixelsize=17:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
@@ -94,7 +95,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 1;
+float alpha = 0.9;
 
 /* Terminal colors (16 first used in escape sequence) */
 
